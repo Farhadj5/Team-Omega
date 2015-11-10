8cc8f909fd66dfe8fcbbd130f010f2084b91383b
J-Weigand <Joshua@Joshuas-MacBook-Pro.local>
2015-11-10 12:20:12 -0600

    Skeleton Code
    Structure/Outline pulled from Clark’s header file. Let the games begin!

---
+++ cs2123p5.c
@@ -0,0 +1,196 @@
+/**********************************************************************
+ cs2123p5.c by TEAM OMEGA [Farhad Jijina, Lane Burris, Joshua Weigand]
+ 
+ Purpose:
+ 
+ Command Parameters:
+ 
+ Input:
+ 
+ Results:
+ 
+ Returns:
+ 
+ Notes:
+ 
+ **********************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p5Driver.c"
+
+
+/**************************************************************************
+                            Helper Functions
+ ***************************************************************************/
+
+//Insert Helper Functions to be used with with main Functions found in the
+//  Header file.
+
+
+
+
+
+
+
+
+/**************************************************************************
+            Main Functions (NOTE: Helper Functions Listed Above)
+ ***************************************************************************/
+
+/******************************* findId ***********************************
+ NodeT *findId(NodeT *p, char szId[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+
+ **************************************************************************/
+NodeT *findId(NodeT *p, char szId[])
+{
+    
+    
+}
+
+/***************************** findParent *********************************
+ NodeT *findParent(NodeT *pParent, NodeT *p, NodeT *pkid)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+NodeT *findParent(NodeT *pParent, NodeT *p, NodeT *pkid)
+{
+    
+    
+}
+
+/*************************** printPriceMenu *******************************
+ void printPriceMenu(Tree tree)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void printPriceMenu(Tree tree)
+{
+    
+    
+}
+
+/****************************** printOne **********************************
+ void printOne(Tree tree, char szId[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void printOne(Tree tree, char szId[])
+{
+    
+    
+}
+
+/****************************** freeSubTree *******************************
+ void freeSubTree(NodeT *p)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void freeSubTree(NodeT *p)
+{
+    
+    
+}
+
+/********************************* freeTree *******************************
+ void freeTree(Tree tree)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void freeTree(Tree tree)
+{
+    
+    
+}
+
+/************************** insertPriceMenu *******************************
+ void insertPriceMenu(Tree tree, Element element, char szParentId[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void insertPriceMenu(Tree tree, Element element, char szParentId[])
+{
+    
+    
+}
+
+/************************** determineQuote *******************************
+ QuoteResult determineQuote(Tree tree, QuoteSelection quoteSelection)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+QuoteResult determineQuote(Tree tree, QuoteSelection quoteSelection)
+{
+    
+    
+}
+
+/********************************* deleteItem *******************************
+ void deleteItem(Tree tree, char szId[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void deleteItem(Tree tree, char szId[])
+{
+    
+    
+}
+
+/**************************************************************************
+    Driver function to process a input data line (Include in Driver?)
+ ***************************************************************************/
+
+/***************************** processCommand *****************************
+ void processCommand(Tree tree, QuoteSelection quote, char szInputBuffer[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void processCommand(Tree tree, QuoteSelection quote, char szInputBuffer[])
+{
+    
+    
+}
+
+
