07105010b81a159296885f564443d8939503be58
J-Weigand <Joshua@Joshuas-MacBook-Pro.local>
2015-11-19 14:10:25 -0600

    Added Helper/Main Functions
    Added: printPriceMenu
    Added: prettyPrintMenu

--- cs2123p5.c
+++ cs2123p5.c
@@ -25,8 +25,41 @@
                             Helper Functions
  ***************************************************************************/
 
-//Insert Helper Functions to be used with with main Functions found in the
-//  Header file.
+/**************************** prettyPrintMenu *****************************
+ void prettyPrintMenu(NodeT *p, int iIndent)
+ Purpose:
+    Traverses the tree, prints out the siblings, and their details in the
+ form of a price menu.
+ Parameters:
+ I      NodeT *p        Pointer directed to the root of the Tree
+ I      int Indent       Used to determine starting point of indented print
+ Returns:
+ N/A
+ **************************************************************************/
+void prettyPrintMenu(NodeT *p, int iIndent)
+{
+    //Local Variables
+    int i;
+    NodeT *pTitle = p->element.szTitle;
+    NodeT *pCost = p->element.dCost;
+    NodeT *pCostIdn = p->element.cCostInd;
+
+    if (p == NULL)
+        return;
+    for (i = 0; i < iIndent; i++)
+      printf("   ");
+      if(pCostIdn == 0) //Prints the szTitle of each node NOT containing a price
+         printf("  %2s\n", *pTitle);
+      else              //Prints the szTitle of each node containing a price
+      {
+        printf("  %2s", *pTitle);
+        printf("\t %d", *pCost); //Still Needs Work (Allignment)
+      }
+
+    prettyPrintT(p->pChild,iIndent+1);
+    prettyPrintT(p->pSibling,iIndent+1);
+}
+
 
 
 
@@ -110,8 +143,14 @@ NodeT *findParent(NodeT *pParent, NodeT *p, NodeT *pkid)
  **************************************************************************/
 void printPriceMenu(Tree tree)
 {
-    
-    
+    //Local Variable(s)
+    Node *pCurrentNode = tree->pRoot;
+
+    //Price Menu Title
+    printf("Price Menu\n");
+
+    //Traverse the tree and print each nodes Title
+    prettyPrintMenu(*pCurrentNode, 1); //Start indents = 1
 }
 
 /****************************** printOne **********************************
