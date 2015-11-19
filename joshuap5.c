b59c55f072788572b635d63eee55842cfa4eed71
J-Weigand <Joshua@Joshuas-MacBook-Pro.local>
2015-11-19 14:16:43 -0600

    Added joshuap5.c

---
+++ joshuap5.c
@@ -0,0 +1,168 @@
+/**************************************************************************
+                            Helper Functions
+ ***************************************************************************/
+
+/**************************** prettyPrintMenu *****************************
+ void prettyPrintMenu(NodeT *p, int iIndent)
+ Purpose:
+    Traverses the tree, prints out the siblings, and their details in the
+ form of a price menu.
+ Parameters:
+ I      NodeT *p        Pointer directed to the root of the Tree
+ I      int Indent       Used to determine starting point of indented print
+ Returns:
+ N/A
+ **************************************************************************/
+void prettyPrintMenu(NodeT *p, int iIndent)
+{
+    //Local Variables
+    int i;
+    NodeT *pTitle = p->element.szTitle;
+    NodeT *pCost = p->element.dCost;
+    NodeT *pCostIdn = p->element.cCostInd;
+
+    if (p == NULL)
+        return;
+    for (i = 0; i < iIndent; i++)
+      printf("   ");
+      if(pCostIdn == 0) //Prints the szTitle of each node NOT containing a price
+         printf("  %2s\n", *pTitle);
+      else              //Prints the szTitle of each node containing a price
+      {
+        printf("  %2s", *pTitle);
+        printf("\t %d", *pCost); //Still Needs Work (Allignment)
+      }
+
+    prettyPrintT(p->pChild,iIndent+1);
+    prettyPrintT(p->pSibling,iIndent+1);
+}
+
+/**************************************************************************
+                             Main Functions
+ ***************************************************************************/
+/*************************** printPriceMenu *******************************
+ void printPriceMenu(Tree tree)
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************/
+void printPriceMenu(Tree tree)
+{
+    //Local Variable(s)
+    Node *pCurrentNode = tree->pRoot;
+
+    //Price Menu Title
+    printf("Price Menu\n");
+
+    //Traverse the tree and print each nodes Title
+    prettyPrintMenu(*pCurrentNode, 1); //Start indents = 1
+}
+
+/****************************** printOne **********************************
+ void printOne(Tree tree, char szId[])
+ Purpose:
+    Supposed to print only one part of the tree
+        - Title
+        - Cost
+ Parameters:
+ I      Tree tree       Struct containing the Root and Nodes of a tree
+ IO     char szId[]     Element Identification
+ Returns:
+ 
+**************************************************************************/
+void printOne(Tree tree, char szId[])
+{
+    NodeT *p = findId(tree->pRoot,szId);
+    if (p == NULL)
+        printf("PRINT ERROR: Id %s not found\n", szId);
+    else
+        printf("PRINT ONE:\n Title: %s Cost: %lf\n"
+                ,p->element.szTitle
+                ,p->element.dCost);
+}
+
+/**************************************************************************
+                                    QuoteEnd
+ ***************************************************************************/
+
+/******************************** quoteEND ********************************
+ void quoteEND(Tree tree, char szId[])
+ Purpose:
+ 
+ Parameters:
+ 
+ Returns:
+ 
+ **************************************************************************
+void quoteEND(Tree tree, char szId[])
+{
+    //Local Variable Declaration
+    char szTitle[30] = tree->pRoot->element.szTitle;
+    
+    //Print Model
+    while(szId == model)
+    {
+        if(tree->pRoot->element.szTitle == base) //Prints BASE Model
+            printf("\t Base
+                   \t\t\t\t
+                   
+                   else if(tree->pRoot->element.szTitle == lx) //Prints PLUS Model
+                   printf("\t Plus\n");
+                   else if(tree->pRoot->element.szTitle == oy) //Prints OH_YEAH! Model
+                   printf("\t Oh_Yeah!\n");
+                   else
+                   printf("\nInvalid Model\n");
+                   }
+                   
+                   //Print Engine
+                   while(szId == engine)
+                   {
+                       if(tree->pRoot->element.szId == base) //Prints BASE Model
+                           if
+                               printf("\t\t\t Base\n");
+                       
+                           else
+                               printf("\nInvalid Engine\n");
+                       
+                   }
+                   
+                   //Print Color
+                   while(szId == color)
+                   {
+                       if(tree->pRoot->element.szId == base) //Prints BASE Model
+                           if
+                               printf("\t\t\t Base\n");
+                       
+                           else
+                               printf("\nInvalid Engine\n");
+                       
+                   }
+                   
+                   //Print Audio
+                   while(szId == audio)
+                   {
+                       if(tree->pRoot->element.szId == base) //Prints BASE Model
+                           if
+                               printf("\t\t\t Base\n");
+                       
+                           else
+                               printf("\nInvalid Engine\n");
+                       
+                   }
+                   
+                   //Print Warranty
+                   while(szId == warranty)
+                   {
+                       if(tree->pRoot->element.szId == base) //Prints BASE Model
+                           if
+                               printf("\t\t\t Base\n");
+                       
+                           else
+                               printf("\nInvalid Engine\n");
+                       
+                   }
+        }
+*/
