9f8a109e02d9fcbd351ebb4586ffe34eb27056bc
tlburris3 <tlburris3@yahoo.com>
2015-11-20 13:03:45 -0600

    Lane's file

---
+++ Lanep5.c
@@ -0,0 +1,89 @@
+/**********************************************************************
+ cs2123p5.c by TEAM OMEGA [Farhad Jijina, Lane Burris, Joshua Weigand]
+ 
+ Purpose:
+ 
+ Command Parameters:
+ 
+ Input:
+ 
+ Results:
+ 
+ Returns:
+ 
+ Notes:
+ 
+ **********************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p5.h"
+
+/****************************** freeSubTree *******************************
+ void freeSubTree(NodeT *p)
+ Purpose:
+ Frees the current node and its children.
+ Parameters:
+ I NodeT *p    Node that will be freed.
+ Returns:
+ 
+ **************************************************************************/
+void freeSubTree(NodeT *p)
+{
+    
+    if (p == NULL)
+    {
+        return;
+    }
+    
+    freeTree(p->pChild);
+    freeTree(p->pSibling);
+    free(p);
+    
+}
+
+/********************************* freeTree *******************************
+ void freeTree(Tree tree)
+ Purpose:
+ Frees the whole tree.
+ Parameters:
+ I Tree tree     Tree that is being passed in for freeing.
+ Returns:
+ **************************************************************************/
+void freeTree(Tree tree)
+{
+    
+    NodeT *p;
+    p = tree->pRoot;
+    
+    freeSubTree(p);
+    
+}
+
+/********************************* deleteItem *******************************
+ void deleteItem(Tree tree, char szId[])
+ Purpose:
+ Uses freeSubTree to free current node as well as the children of the current node.
+ After freeSubTree is used, reconnects that sub-tree's sibiling to the parent, or to the other sub-tree's sibiling.
+ Parameters:
+ I/O Tree tree
+ Returns:
+ 
+ **************************************************************************/
+void deleteItem(Tree tree, char szId[])
+{
+    NodeT *p;
+    NodeT *pKid = findParent(NULL, p-pRoot, pKid);
+    
+    if (*p == pKid)
+    {
+        *p = pKid->pSibling;
+        pKid->pSibling = NULL;
+        freeSubTree(pKid);
+    }
+    else
+    {
+        
+    }
+}
